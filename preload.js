8af45a9a44bf75a28ae073c2a64596d2d5ec8c11
aryansxs9-crypto <aryansxs9@gmail.com>
2026-08-25 21:45:23 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,11 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('launcherAPI', {
-  launchGame: (options) => ipcRenderer.send('launch-game', options),
-  openFolder: () => ipcRenderer.send('open-game-folder'),
   minimize: () => ipcRenderer.send('window-minimize'),
   maximize: () => ipcRenderer.send('window-maximize'),
   close: () => ipcRenderer.send('window-close'),
+  openFolder: () => ipcRenderer.send('open-game-folder'),
+  launchGame: (config) => ipcRenderer.send('launch-game', config),
+  onStatus: (callback) => ipcRenderer.on('game-status', (event, status) => callback(status)),
   onLog: (callback) => ipcRenderer.on('game-log', (event, data) => callback(data)),
-  onStatus: (callback) => ipcRenderer.on('game-status', (event, data) => callback(data))
+  onUpdaterLog: (callback) => ipcRenderer.on('updater-log', (event, message) => callback(message))
 });
