0fa1a64f7b6fd0d1939ab7a46a772f42dd9b11c8
aryansxs9-crypto <aryansxs9@gmail.com>
2026-08-26 15:45:28 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -8,5 +8,11 @@ contextBridge.exposeInMainWorld('launcherAPI', {
   launchGame: (config) => ipcRenderer.send('launch-game', config),
   onStatus: (callback) => ipcRenderer.on('game-status', (event, status) => callback(status)),
   onLog: (callback) => ipcRenderer.on('game-log', (event, data) => callback(data)),
-  onUpdaterLog: (callback) => ipcRenderer.on('updater-log', (event, message) => callback(message))
+  onUpdaterLog: (callback) => ipcRenderer.on('updater-log', (event, message) => callback(message)),
+
+  // Mod Hub APIs
+  searchMods: (query, version, loader) => ipcRenderer.invoke('search-mods', { query, version, loader }),
+  installMod: (projectId, version, loader) => ipcRenderer.invoke('install-mod', { projectId, version, loader }),
+  getInstalledMods: () => ipcRenderer.invoke('get-installed-mods'),
+  deleteMod: (filename) => ipcRenderer.invoke('delete-mod', filename)
 });
