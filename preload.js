2b200cd920527063c28255f4933ab9ef9014be2a
aryansxs9-crypto <aryansxs9@gmail.com>
2026-08-27 22:52:52 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,13 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimizeWindow: () => ipcRenderer.send('window-minimize'),
-  maximizeWindow: () => ipcRenderer.send('window-maximize'),
-  closeWindow: () => ipcRenderer.send('window-close'),
-  openExternal: (url) => ipcRenderer.send('open-external', url),
-  openGameFolder: () => ipcRenderer.send('open-game-folder'),
+contextBridge.exposeInMainWorld('launcherAPI', {
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close'),
+  openFolder: () => ipcRenderer.send('open-game-folder'),
+  openLink: (url) => ipcRenderer.send('open-external', url),
   launchGame: (config) => ipcRenderer.send('launch-game', config),
-  onGameLog: (callback) => ipcRenderer.on('game-log', (event, value) => callback(value)),
-  onGameStatus: (callback) => ipcRenderer.on('game-status', (event, value) => callback(value)),
-  onUpdateMessage: (callback) => ipcRenderer.on('update-message', (event, value) => callback(value))
+  getInstalledMods: () => ipcRenderer.invoke('get-installed-mods'),
+  searchMods: (query, version, loader) => ipcRenderer.invoke('search-mods', { query, version, loader }),
+  installMod: (projectId, version, loader) => ipcRenderer.invoke('install-mod', { projectId, version, loader }),
+  deleteMod: (filename) => ipcRenderer.invoke('delete-mod', filename),
+  onLog: (callback) => ipcRenderer.on('game-log', (event, value) => callback(value)),
+  onStatus: (callback) => ipcRenderer.on('game-status', (event, value) => callback(value)),
+  onUpdaterLog: (callback) => ipcRenderer.on('update-message', (event, value) => callback(value))
 });
